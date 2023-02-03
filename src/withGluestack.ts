0feda97e60ed11ed2950c1c7d637bf984db5a217
mayank-96 <pagarmayank07@gmail.com>
2023-02-03 15:16:20 +0530

    fix: changed api

--- src/withGluestack.ts
+++ src/withGluestack.ts
@@ -1,55 +1,48 @@
-type ConfigType = {
-  transpileModules: Array<any>;
-  plugins: Array<any>;
-  nextConfig: any;
-};
-
-type withGluestackParam = {
-  config: ConfigType;
-  phase?: Array<any>;
-};
-
-export default function withGluestack(
-  config: ConfigType = { transpileModules: [], plugins: [], nextConfig: {} },
-  phase: Array<any> = []
-): withGluestackParam {
-  let transpileModules = [
-    'react-native-web',
-    '@dank-style/react',
-    '@dank-style/css-injector',
-    '@gluestack/ui-next-adapter',
-  ];
-
-  if (config.transpileModules !== undefined) {
-    transpileModules = [...transpileModules, ...config.transpileModules];
-  }
+export default function withGluestackUI({ transpileModules, nextConfig }: any) {
+  let gluestackUITranspileModules = Array.from(
+    new Set([
+      'react-native-web',
+      '@dank-style/react',
+      '@dank-style/css-injector',
+      '@gluestack/ui-next-adapter',
+      '@universa11y/provider',
+      '@react-native-aria/overlays',
+      '@universa11y/overlay',
+      '@react-native-aria/utils',
+      '@universa11y/react-native-aria',
+      '@universa11y/toast',
+      '@universa11y/transitions',
+      ...transpileModules,
+    ])
+  );
 
   const withPlugins = require('next-compose-plugins');
-  const withTM = require('next-transpile-modules')(transpileModules);
-  return withPlugins(
-    [withTM, ...(config.plugins || [])],
-    {
-      webpack: (config: any) => {
-        config.resolve.alias = {
-          ...(config.resolve.alias || {}),
-          'react-native$': 'react-native-web',
-        };
-        config.resolve.extensions = [
-          '.web.js',
-          '.web.ts',
-          '.web.tsx',
-          ...config.resolve.extensions,
-        ];
+  const withTM = require('next-transpile-modules')([
+    ...gluestackUITranspileModules,
+  ]);
 
-        config.module.rules.push({
-          test: /\.ttf$/,
-          loader: 'url-loader',
-        });
+  const updatedNextConfig = {
+    ...nextConfig,
+    webpack: (config: any) => {
+      config.resolve.alias = {
+        ...(config.resolve.alias || {}),
+        'react-native$': 'react-native-web',
+      };
+      config.resolve.extensions = [
+        '.web.js',
+        '.web.ts',
+        '.web.tsx',
+        ...config.resolve.extensions,
+      ];
 
-        return config;
-      },
-      ...(config.nextConfig && config.nextConfig),
+      config.module.rules.push({
+        test: /\.ttf$/,
+        loader: 'url-loader',
+      });
+
+      return config;
     },
-    [...phase]
-  );
+  };
+
+  return withPlugins([withTM], updatedNextConfig);
 }
